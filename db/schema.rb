608739a682a0d673b169d92ede3317258ee96447
jy <ailinjulygao@gmail.com>
2017-05-04 11:08:22 +0800

    new&edit adjustment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501052711) do
+ActiveRecord::Schema.define(version: 20170503212911) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20170501052711) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "attachment"
+    t.string   "user_name"
   end
 
   create_table "users", force: :cascade do |t|
